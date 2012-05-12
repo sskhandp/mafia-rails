2d7a19e127dc8dc2eabed40032eefd27682f2865
Gurrinder Kharbanda <gurrinder.kharbanda@involver.com>
2012-05-11 21:38:25 -0700

    Bootstrapped new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MafiaRails::Application.config.secret_token = '3fae1a5da24f89b743427220f9d024838add142cb0711013d7923dbc09637ee891463d5f24ebbb53a30d8e97617c3b62f0752b5e276f88300b30820e49f4b571'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MafiaRails::Application.config.session_store :cookie_store, key: '_mafia-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MafiaRails::Application.config.session_store :active_record_store
