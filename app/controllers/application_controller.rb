efe9a50a2c0f73bfd27db7838d4b31c0af5bc31b
Gurrinder Kharbanda <gurrinder.kharbanda@involver.com>
2012-05-20 19:25:38 -0700

    Skip verify authernticity token for json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
 
+  def verified_request?
+    if request.content_type == "application/json"
+      true
+    else
+      super()
+    end
+  end
+
   private
 
   def current_user
